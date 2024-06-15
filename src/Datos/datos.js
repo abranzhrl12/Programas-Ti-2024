a2a5008b83c7bf43c52ff29b8aa11a3d0af8adff
abranzhrl12 <127367731+abranzhrl12@users.noreply.github.com>
2024-06-14 20:28:57 -0500

    se agrego el tema de paginacion y rutas

--- src/Datos/datos.js
+++ src/Datos/datos.js
@@ -3,42 +3,74 @@ export default {
     Edicion: [
       {
         id: 1,
-        nombre: "Adobe Ilustrator",
+        nombre: "Adobe Ilustrator 2024",
         imagen: "/imagenes/ilustrator22 (1).avif",
-        Descripcion: "lorem lorem lorem lorem lorem lorem",
+        Descripcion:
+          "Ilustraciones profesionales con Illustrator. Crea diseños impactantes con herramientas intuitivas y versátiles.",
         Caracteristica: "lorem lorem",
         Video: "/aaaaa",
-        link_drive: "https//google.com",
+        link_drive:
+          "https://drive.google.com/file/d/1CQ0R679G-0wJPPcccTd0kqvTYjGkq4GD/view?usp=sharing",
         link_mediafire: "https://google.com",
       },
       {
         id: 2,
-        nombre: "Adobe Photoshop",
+        nombre: "Adobe Photoshop 2024",
         imagen: "/imagenes/potoshop.webp",
-        Descripcion: "lorem lorem lorem lorem lorem lorem",
+        Descripcion:
+          "Potente herramienta de edición de imágenes: Photoshop. Transforma tus fotos con creatividad y precisión",
         Caracteristica: "lorem lorem",
         Video: "/aaaaa",
-        link_drive: "https//google.com",
+        link_drive:
+          "https://drive.google.com/file/d/1wBvBgOHLs4GH7pvpvyvMAxOdliRR_tvm/view?usp=sharing",
         link_mediafire: "https://google.com",
       },
       {
         id: 3,
-        nombre: "Corel draw",
-        imagen: "/imagenes/coreldraww.webp",
-        Descripcion: "lorem lorem lorem lorem lorem lorem",
+        nombre: "Corel draw 2024",
+        imagen: "/imagenes/coreldrawavi.avif",
+        Descripcion:
+          "Diseño sin límites con CorelDRAW. Libera tu creatividad y lleva tus ideas a otro nivel con esta potente herramienta de diseño gráfico.",
         Caracteristica: "lorem lorem",
         Video: "/aaaaa",
-        link_drive: "https//google.com",
+        link_drive:
+          "https://drive.google.com/file/d/1AJSdfCK7ZWSqsSz4OtAst37JK5l977NF/view?usp=sharing",
         link_mediafire: "https://google.com",
       },
       {
         id: 4,
         nombre: "Luminar 2024",
         imagen: "/imagenes/lumina (2).webp",
-        Descripcion: "lorem lorem lorem lorem lorem lorem",
+        Descripcion:
+          "Iluminación perfecta con Luminar Neo. Transforma tus fotos con tecnología de vanguardia y efectos sorprendentes.",
         Caracteristica: "lorem lorem",
         Video: "/aaaaa",
-        link_drive: "https//google.com",
+        link_drive:
+          "https://drive.google.com/file/d/1FJro8-hTz64S3NGXhV-4EKVMDZhxtRXa/view?usp=sharing",
+        link_mediafire: "https://google.com",
+      },
+      {
+        id: 5,
+        nombre: "Filmora 13",
+        imagen: "/imagenes/filmora13.webp",
+        Descripcion:
+          "Crea y edita vídeos como un profesional con Filmora. Añade efectos, música y transiciones para dar vida a tus historias de manera fácil y divertida.",
+        Caracteristica: "lorem lorem",
+        Video: "/aaaaa",
+        link_drive:
+          "https://drive.google.com/file/d/1JJuMh2q1PfT6qgnLuk5KKvlTtpVvywVN/view?usp=sharing",
+        link_mediafire: "https://google.com",
+      },
+      {
+        id: 6,
+        nombre: "Wondershare EdrawMax 13",
+        imagen: "/imagenes/edrawmax.webp",
+        Descripcion:
+          "EdrawMax 13 es un software de diagramación y diseño versátil y fácil de usar.",
+        Caracteristica: "lorem lorem",
+        Video: "/aaaaa",
+        link_drive:
+          "https://drive.google.com/file/d/1SKJlaPntQmhtB1Wxb-S853GsJEX0W9Ol/view?usp=sharing",
         link_mediafire: "https://google.com",
       },
     ],
@@ -47,11 +79,13 @@ export default {
         id: 1,
         nombre: "Windows 11",
         imagen: "/imagenes/win11.webp",
-        Descripcion: "lorem lorem lorem lorem lorem lorem",
+        Descripcion:
+          "La nueva era de la productividad y la innovación. Experimenta un sistema operativo moderno, elegante y eficiente para todas tus necesidades digitales.",
         Caracteristica: "lorem lorem",
         Video: "/aaaaa",
-        link_drive: "https//google.com",
-        link_mediafire: "https://google.com",
+        link_drive:
+          "https://drive.google.com/file/d/1AtsbkhkgvomE8YIALTJ3UqgCg1b3WGTS/view?usp=sharing",
+        link_mediafire: "fdsfdsfd",
       },
     ],
   },
