6d8ba48f0303078a854c3e3841e142653fe75e02
abranzhrl12 <127367731+abranzhrl12@users.noreply.github.com>
2024-06-26 23:22:06 -0500

    se agrego para no poder ver fuente pagina

--- src/pelis.js
+++ src/pelis.js
@@ -3,6 +3,14 @@ import datosPelis from "./Datos/datosPelis";
 document.addEventListener('contextmenu', function(e) {
 e.preventDefault();
 })
+document.addEventListener('keydown', function(e) {
+    // Desactivar la tecla F12
+    if (e.key === 'F12' || e.keyCode === 123) {
+        e.preventDefault();
+        return false;
+    }
+});
+
 const peliculas = datosPelis.Peliculas.Extreno;
 
 function generarPlantillaPelicula(pelicula) {
